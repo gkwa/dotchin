b767656af3a6c75303b0b63a21fa67a2a2269f1a
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-22 11:06:23 -0700

    Ok

--- dotchin.go
+++ dotchin.go
@@ -1,49 +1,128 @@
 package dotchin
 
 import (
+	"encoding/json"
 	"log/slog"
+	"math/rand"
+	"sync"
+	"time"
 
 	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/taylormonacelli/lemondrop"
 )
 
 func Main() int {
 	slog.Debug("dotchin", "test", true)
-	test()
+
+	regionDetails, err := lemondrop.GetRegionDetails()
+	if err != nil {
+		slog.Error("get regions", "error", err)
+	}
+
+	regionNames := make([]string, 0)
+	for _, region := range regionDetails {
+		regionNames = append(regionNames, region.RegionCode)
+	}
+
+	// regions := chooseRandomItem(regionNames, 100)
+	regions := regionNames
+	slog.Debug("searching regions", "regions", regions)
+
+	result := GetInstanceTypesAvailableInRegions(regions)
+
+	indentedJSON, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		slog.Error("marshalling", "error", err)
+		return 1
+	}
+
+	fmt.Println(string(indentedJSON))
 	return 0
 }
 
-func test() {
-	// regions,err := lemondrop.GetRegionDetails()
-	// if err != nil {
-	// 	slog.Error("fetching aws regions","error",err)
-	// 	return
-	// }
+func chooseRandomItem(items []string, count int) []string {
+	seed := time.Now().UnixNano()
+	rng := rand.New(rand.NewSource(seed))
 
-	region := "us-west-2"
+	if count > len(items) {
+		count = len(items)
+	}
+
+	rng.Shuffle(len(items), func(i, j int) {
+		items[i], items[j] = items[j], items[i]
+	})
+
+	randomSlice := items[:count]
+
+	return randomSlice
+}
+
+func GetInstanceTypesAvailableInRegions(regions []string) map[string][]types.InstanceTypeInfo {
+	concurrencyLimit := 100
+	semaphoreChan := make(chan struct{}, concurrencyLimit)
+	defer close(semaphoreChan)
+	wg := sync.WaitGroup{}
+
+	results := make(chan []types.InstanceTypeInfo, len(regions))
+	x := make(map[string][]types.InstanceTypeInfo)
+
+	for _, region := range regions {
+		wg.Add(1)
+		semaphoreChan <- struct{}{} // Acquire semaphore
+		go func(region string) {
+			defer func() {
+				<-semaphoreChan // Release semaphore
+				wg.Done()
+			}()
+
+			output, err := GetInstanceTypesAvailableInRegion(region)
+			if err != nil {
+				slog.Error("GetInstanceTypesAvailableInRegion", "error", err)
+				return
+			}
+			x[region] = output
+
+			slog.Debug("instance metrics", "region", region, "count", len(output))
+
+			results <- output
+		}(region)
+	}
+
+	wg.Wait()
+	close(results)
+
+	return x
+}
+
+func GetInstanceTypesAvailableInRegion(region string) ([]types.InstanceTypeInfo, error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		fmt.Printf("Error loading AWS SDK configuration: %v\n", err)
-		return
+		return []types.InstanceTypeInfo{}, err
 	}
 
 	client := ec2.NewFromConfig(cfg)
 
-	// Create an input for DescribeInstanceTypes operation.
 	input := &ec2.DescribeInstanceTypesInput{}
 
-	resp, err := client.DescribeInstanceTypes(context.TODO(), input)
-	if err != nil {
-		fmt.Printf("Error describing instance types: %v\n", err)
-		return
+	var allInstanceTypes []types.InstanceTypeInfo
+
+	paginator := ec2.NewDescribeInstanceTypesPaginator(client, input)
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(context.TODO())
+		if err != nil {
+			slog.Error("error describing instance types", "error", err)
+			return []types.InstanceTypeInfo{}, err
+		}
+
+		allInstanceTypes = append(allInstanceTypes, page.InstanceTypes...)
 	}
 
-	// Iterate over the instance types and print their details.
-	for _, instanceType := range resp.InstanceTypes {
-		fmt.Printf("Instance Type: %s\n", instanceType.InstanceType)
-		fmt.Println()
-	}
+	return allInstanceTypes, nil
 }
--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/taylormonacelli/dotchin
 
-go 1.21
-
-toolchain go1.21.1
+go 1.21.1
 
 require (
 	github.com/aws/aws-sdk-go-v2/config v1.19.0
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.126.0
 	github.com/taylormonacelli/goldbug v0.0.6
+	github.com/taylormonacelli/lemondrop v0.0.15
 )
 
 require (
+	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.21.2 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.43 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.13 // indirect
@@ -18,9 +18,14 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.37 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.45 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.37 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.38.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.15.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.17.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.23.2 // indirect
 	github.com/aws/smithy-go v1.15.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/taylormonacelli/forestfish v0.0.2 // indirect
+	github.com/taylormonacelli/somespider v0.0.0-20231012013812-aa3606dd52d2 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
